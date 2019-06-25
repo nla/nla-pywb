cb35f11b05a276e9db6cef70ad3d730b2dc847e8
Alex Osborne <aosborne@nla.gov.au>
2019-06-25 20:07:10 +0900

    sed urls into config

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -6,3 +6,4 @@ virtualenv-3.4 $dest
 $dest/bin/pip install pywb==2.2.20190311
 virtualenv-3.4 --relocatable $dest
 cp -a awa $dest
+sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa/config.yaml
