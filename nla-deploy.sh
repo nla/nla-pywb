6cb4325615ba38718c8f792daa819dddae2d570a
Alex Osborne <aosborne@nla.gov.au>
2020-02-20 13:53:54 +0900

    Try nobanner as a separate process

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -5,5 +5,6 @@ dest=$1
 virtualenv-3.6 $dest
 $dest/bin/pip install pywb==2.2.20190311
 virtualenv-3.6 --relocatable $dest
-cp -a awa $dest
+cp -a awa awa-nobanner $dest
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa/config.yaml
+sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa-nobanner/config.yaml
