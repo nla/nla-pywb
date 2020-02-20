6d843e3890cb49a0072f6fbec4bc034d3cbfc59a
Alex Osborne <aosborne@nla.gov.au>
2020-02-20 12:42:40 +0900

    Switch to python 3.6, pip install now fails on 3.4

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -2,8 +2,8 @@
 set -eu
 
 dest=$1
-virtualenv-3.4 $dest
+virtualenv-3.6 $dest
 $dest/bin/pip install pywb==2.2.20190311
-virtualenv-3.4 --relocatable $dest
+virtualenv-3.6 --relocatable $dest
 cp -a awa $dest
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa/config.yaml
