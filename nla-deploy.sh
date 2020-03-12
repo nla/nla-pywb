02b493583ba187d461dc352c5d80d3f4caaee339
Alex Osborne <aosborne@nla.gov.au>
2020-03-12 19:09:02 +0900

    Use xmlquery protocol to work around issue with spaces URLs

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -3,7 +3,7 @@ set -eu
 
 dest=$1
 virtualenv-3.6 $dest
-$dest/bin/pip install pywb==2.2.20190311
+$dest/bin/pip install git+https://github.com/webrecorder/pywb.git@v-2.4.0-rc2
 virtualenv-3.6 --relocatable $dest
 cp -a awa awa-nobanner $dest
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa/config.yaml
