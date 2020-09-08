8921ce14060465d958ce2873d9b72018ce827ea4
Alex Osborne <aosborne@nla.gov.au>
2020-09-08 21:28:33 +0900

    Add uwsgi

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -4,6 +4,9 @@ set -eu
 dest=$1
 virtualenv-3.6 $dest
 $dest/bin/pip install git+https://github.com/webrecorder/pywb.git@v-2.4.0-rc2
+$dest/bin/pip install wheel
+$dest/bin/pip install uwsgi
+$dest/bin/pip install gevent
 virtualenv-3.6 --relocatable $dest
 cp -a awa awa-nobanner $dest
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa/config.yaml
