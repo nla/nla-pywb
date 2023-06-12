b5da207cd360e11548c90f6851b04cc5aef5d05a
Alex Osborne <aosborne@nla.gov.au>
2023-06-12 15:29:01 +0900

    Update pywb to v-2.7.4-nla1 for testing

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -3,8 +3,8 @@ set -eu
 
 dest=$1
 virtualenv-3.6 $dest
-#$dest/bin/pip install git+https://github.com/nla/pywb.git@v-2.6.0-nla1
-$dest/bin/pip install git+https://github.com/webrecorder/pywb.git@v-2.6.6
+$dest/bin/pip install git+https://github.com/nla/pywb.git@v-2.7.4-nla1
+#$dest/bin/pip install git+https://github.com/webrecorder/pywb.git@v-2.6.6
 $dest/bin/pip install wheel
 $dest/bin/pip install uwsgi
 $dest/bin/pip install gevent
