76e59417bf75d41742a9aa37e42f450a9806d99d
Alex Osborne <aosborne@nla.gov.au>
2024-11-12 16:16:06 +0900

    --relocatable seems to be gone

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -13,7 +13,6 @@ $dest/bin/pip install git+https://github.com/nla/pywb.git
 $dest/bin/pip install wheel
 $dest/bin/pip install uwsgi
 $dest/bin/pip install gevent
-$virtualenv --relocatable $dest
 cp -a awa awa-nobanner $dest
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa/config.yaml
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa-nobanner/config.yaml
