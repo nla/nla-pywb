f324e513b7ed71fb9ec2776c9d0edf053a50a563
Alex Osborne <aosborne@nla.gov.au>
2025-04-02 16:46:51 +0900

    Handle virtualenv that doesn't support --relocatable

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -20,7 +20,7 @@ $dest/bin/pip install git+https://github.com/nla/pywb.git@client-side-playback-s
 $dest/bin/pip install wheel
 $dest/bin/pip install uwsgi
 $dest/bin/pip install gevent
-$virtualenv --relocatable $dest
+$virtualenv --relocatable $dest || echo "virtualenv doesnt support relocatable (probably ok)"
 cp -a awa awa-nobanner $dest
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa/config.yaml
 sed -i -e "s|CDX_URL|$CDX_URL|" -e "s|WARC_URL|$WARC_URL|" $dest/awa-nobanner/config.yaml
